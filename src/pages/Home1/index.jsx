a11fc4938f1f0da3e54630dccad14bbdcec3210d
michael <micheal.emil31@gmail.com>
2024-03-30 12:58:56 +0200

    update home page and home page is done

--- src/pages/Home1/index.jsx
+++ src/pages/Home1/index.jsx
@@ -58,296 +58,117 @@ export default function Home1Page() {
             </Menu>
           </div>
         </Sidebar>
-        <div className="flex flex-col gap-3.5 flex-1">
-          <Heading size="md" as="h1" className="ml-5 mt-5 !font-black">
-            New Reservations
-          </Heading>
-          <div
-            className="new-reservations-container"
-            style={{ width: "100%", maxHeight: "370px", overflowY: "auto", overflowX: "auto" }}
-          >
-            <div className="flex flex-wrap gap-[55px] bg-gray-100_04 pl-[25px] pb-[30px] ">
-              <div className=" mt-[20px] bg-white-A700_01">
-                  <Text className="p-[20px]">
-                    <>
-                      Room ID : 22
-                      <br />
-                      <br />
-                      Start time :10 AM
-                      <br />
-                      <br />
-                      End Time : 12PM
-                      <br />
-                      <br />
-                      Reservation ID : 5
-                      <br />
-                      <br />
-                      Date: 4/4/2024
-                    </>
-                  </Text>
-              </div>
-              <div className=" mt-[20px] bg-white-A700_01">
-                  <Text className="p-[20px]">
-                    <>
-                      Room ID : 22
-                      <br />
-                      <br />
-                      Start time :10 AM
-                      <br />
-                      <br />
-                      End Time : 12PM
-                      <br />
-                      <br />
-                      Reservation ID : 5
-                      <br />
-                      <br />
-                      Date: 4/4/2024
-                    </>
-                  </Text>
-              </div>
-              <div className=" mt-[20px] bg-white-A700_01">
-                  <Text className="p-[20px]">
-                    <>
-                      Room ID : 22
-                      <br />
-                      <br />
-                      Start time :10 AM
-                      <br />
-                      <br />
-                      End Time : 12PM
-                      <br />
-                      <br />
-                      Reservation ID : 5
-                      <br />
-                      <br />
-                      Date: 4/4/2024
-                    </>
-                  </Text>
-              </div>
-              <div className=" mt-[20px] bg-white-A700_01">
-                  <Text className="p-[20px]">
-                    <>
-                      Room ID : 22
-                      <br />
-                      <br />
-                      Start time :10 AM
-                      <br />
-                      <br />
-                      End Time : 12PM
-                      <br />
-                      <br />
-                      Reservation ID : 5
-                      <br />
-                      <br />
-                      Date: 4/4/2024
-                    </>
-                  </Text>
-              </div>
-              <div className=" mt-[20px] bg-white-A700_01">
-                  <Text className="p-[20px]">
-                    <>
-                      Room ID : 22
-                      <br />
-                      <br />
-                      Start time :10 AM
-                      <br />
-                      <br />
-                      End Time : 12PM
-                      <br />
-                      <br />
-                      Reservation ID : 5
-                      <br />
-                      <br />
-                      Date: 4/4/2024
-                    </>
-                  </Text>
-              </div>
-              <div className=" mt-[20px] bg-white-A700_01">
-                  <Text className="p-[20px]">
-                    <>
-                      Room ID : 22
-                      <br />
-                      <br />
-                      Start time :10 AM
-                      <br />
-                      <br />
-                      End Time : 12PM
-                      <br />
-                      <br />
-                      Reservation ID : 5
-                      <br />
-                      <br />
-                      Date: 4/4/2024
-                    </>
-                  </Text>
-              </div>
+        <div className="grid grid-row-4">
+          <div className="">
+            <Heading size="md" as="h1" className="ml-5 mt-5 mb-1 !font-black">
+              New Reservations
+            </Heading>
+            <div className="flex flex-wrap gap-[45px] bg-gray-100_04 pl-[25px] pb-[30px] h-[300px] w-[99%] overflow-y-auto ">
+                {[1, 2, 3, 4, 5, 6].map((index) => (
+                  <div key={index} className="mt-[20px] bg-white-A700_01">
+                    <Text className="p-[20px]">
+                      <>
+                        Room ID : 22
+                        <br />
+                        <br />
+                        Start time :10 AM
+                        <br />
+                        <br />
+                        End Time : 12PM
+                        <br />
+                        <br />
+                        Reservation ID : 5
+                        <br />
+                        <br />
+                        Date: 4/4/2024
+                      </>
+                    </Text>
+                  </div>
+                ))}
             </div>
           </div>
-          <div className="flex flex-row flex-wrap gap-3.5 mt-[20px] bg-gray-100_04 h-[500px] w-[99.8%]">
-            <div className="w-[150px] h-[50px] p-[10px] bg-amber-A700 rounded-[35px] m-[30px]">
-                  <Text>
-                    Idle Rooms
-                  </Text>
+          <div className="">
+            <Heading size="md" as="h1" className="ml-5 mt-5 mb-1 !font-black">
+              Idle Rooms
+            </Heading>
+            <div className="flex flex-wrap gap-[45px] bg-gray-100_04 pl-[25px] pb-[30px] h-[300px] w-[99%] overflow-y-auto ">
+                {[1, 2, 3, 4, 5, 6].map((index) => (
+                  <div key={index} className="mt-[20px] bg-white-A700_01">
+                    <Text className="p-[20px]">
+                      <>
+                        Room ID : 22
+                        <br />
+                        <br />
+                        Start time :10 AM
+                        <br />
+                        <br />
+                        End Time : 12PM
+                        <br />
+                        <br />
+                        Reservation ID : 5
+                        <br />
+                        <br />
+                        Date: 4/4/2024
+                      </>
+                    </Text>
+                  </div>
+                ))}
             </div>
-            <div className="w-[240px] h-[50px] p-[10px] bg-amber-A700 rounded-[35px] m-[30px] ml-[290px]">
-                  <Text>
-                    Current Meetings
-                  </Text>
+          </div>
+          <div className="">
+            <Heading size="md" as="h1" className="ml-5 mt-5 mb-1 !font-black">
+              Current Meetings
+            </Heading>
+            <div className="flex flex-wrap gap-[45px] bg-gray-100_04 pl-[25px] pb-[30px] h-[300px] w-[99%] overflow-y-auto ">
+                {[1, 2, 3, 4, 5, 6].map((index) => (
+                  <div key={index} className="mt-[20px] bg-white-A700_01">
+                    <Text className="p-[20px]">
+                      <>
+                        Room ID : 22
+                        <br />
+                        <br />
+                        Start time :10 AM
+                        <br />
+                        <br />
+                        End Time : 12PM
+                        <br />
+                        <br />
+                        Reservation ID : 5
+                        <br />
+                        <br />
+                        Date: 4/4/2024
+                      </>
+                    </Text>
+                  </div>
+                ))}
             </div>
-            <div className="w-[280px] h-[50px] p-[10px] bg-amber-A700 rounded-[35px] m-[30px] ml-[200px]">
-                  <Text>
-                    Upcomming Meetings
-                  </Text>
-            </div>
-            <div className="flex flex-col gap-[30px] w-[28%] h-[75%]	ml-[30px] mt-[-65px] p-[15px] bg-white-A700_01 overflow-y-auto">
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-            </div>
-            <div className="flex flex-col gap-[30px] w-[28%] h-[75%]	ml-[30px] mt-[-65px] p-[15px] bg-white-A700_01 overflow-y-auto">
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-            </div>
-            <div className="flex flex-col gap-[30px] w-[28%] h-[75%]	ml-[30px] mt-[-65px] p-[15px] bg-white-A700_01 overflow-y-auto">
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
-              <div className="border-stone-950 border-b-8 ">
-                <Text>
-                  <>
-                    Room ID : 22
-                    <br />
-                    <br />
-                    Start time :10 AM
-                    <br />
-                    <br />
-                    End Time : 12PM
-                    <br />
-                    <br />
-                    Reservation ID : 5
-                  </>
-                </Text>
-              </div>
+          </div>
+          <div className="">
+            <Heading size="md" as="h1" className="ml-5 mt-5 mb-1 !font-black">
+              Upcoming Meetings
+            </Heading>
+            <div className="flex flex-wrap gap-[45px] bg-gray-100_04 pl-[25px] pb-[30px] h-[300px] w-[99%] overflow-y-auto ">
+                {[1, 2, 3, 4, 5, 6].map((index) => (
+                  <div key={index} className="mt-[20px] bg-white-A700_01">
+                    <Text className="p-[20px]">
+                      <>
+                        Room ID : 22
+                        <br />
+                        <br />
+                        Start time :10 AM
+                        <br />
+                        <br />
+                        End Time : 12PM
+                        <br />
+                        <br />
+                        Reservation ID : 5
+                        <br />
+                        <br />
+                        Date: 4/4/2024
+                      </>
+                    </Text>
+                  </div>
+                ))}
             </div>
           </div>
         </div>
